800f836c8a4dbe17dc0027f466be7c0cff70c807
SLOBY <szabolcs.maj@gmail.com>
2014-02-17 01:37:01 +0100

    Added comments

--- coursera_machine_learning/ex7/pca.m
+++ coursera_machine_learning/ex7/pca.m
@@ -20,9 +20,23 @@ S = zeros(n);
 %       number of examples).
 %
 
+% Covariance matrix
 Sigma = X' * X / m;
 
+% Compute 'eigenvectors' of Sigma by using Singular Value Decomposition
 [U, S, ~] = svd(Sigma);
+
+% U is a n * n matrix that contains all the needed u vectors 
+% (u is a column vector). Usually we take the first K 
+% (number of principal component) columns that we want the reduce 
+% our training example to
+
+% We should choose K depending on how big the retained variance is after
+% we applied PCA. We can calculate the retained variance with the following:
+% Returned matrix S from svd looks like this:
+% [S11,0,0;0,S22,0;0,0,S33]
+% We should ensure that the sum of Sii where i=1 to K
+% divided by Sii where i=1 to n is bigger then 0.99 (if we aim for 99% retained variance)
 
 % =========================================================================
 
